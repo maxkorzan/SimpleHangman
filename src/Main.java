09eaad7c0532c98380727fd63de2a2fe148885f0
maxkorzan <maxkorzan@gmail.com>
2020-01-29 17:16:47 -0500

    updating EOD

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,20 +11,67 @@ public class Main {
 
         //randomly choose a word to guess
         String word = words.get(randomNumber(words.size()));
+        char[] wordArray = word.toCharArray();
+        char[] answerArray = wordArray;
+        int attempts=0;
+        int attemptsLimit=6;
+
+        //test wordArray
+        for(char c : wordArray){
+            System.out.print(c + " ");
+        }
+        System.out.println();
+
+        //create blank array
+        char[] blankArray = new char[wordArray.length];
+        for(int i=0; i<blankArray.length; i++) {
+            blankArray[i] = '_';
+        }
 
         System.out.println("Welcome, let's play hangman!");
-        System.out.println("Here is the word I am thinking of:");
-
-        System.out.print("Enter your guess: ");
-        System.out.println("Your guess so far: _ _ _ _");
-
-//        ^^^Repeat^^^
-//        System.out.print("Enter your guess: ");
-//        System.out.println("You have guessed incorrectly 1/X times.");
-//        System.out.println("Your guess so far: a _ _ _ a _");
+        System.out.print("Here is the word I am thinking of: ");
+        for(char c : blankArray){
+            System.out.print(c+" ");
+        }
+        System.out.println();
 
 
+        while(!Arrays.equals(blankArray, answerArray) && attempts<attemptsLimit){
 
+            System.out.print("Enter your guess: ");
+            String guess = sc.next();
+            char[] guessArray = guess.toCharArray();
+
+            System.out.print("Your guess so far: ");
+            for (int i=0; i < guessArray.length; i++) {
+                for (int j=0; j < answerArray.length; j++) {
+                    if (guessArray[i]==answerArray[j]) {
+                        blankArray[j] =answerArray[j];
+                    }
+                }
+
+
+//                for(char c: guessArray){
+//                    if(c.equals(answerArray[i])){
+//                        //return true;
+//                }
+//                //return false;
+//
+//                if(!answerArray.contains(guessArray[i]) {
+//                    attempts += 1;
+//                }
+            }
+            for(char c : blankArray){
+                System.out.print(c + " ");
+            }
+            System.out.println();
+
+
+            System.out.println("You have guessed incorrectly "+attempts+"/"+attemptsLimit+" times");
+
+        }
+
+        System.out.println("Congratulations! You guessed the word");
 
     }
 
@@ -37,6 +81,12 @@ public class Main {
         int randnum = rand.nextInt(x);
         return randnum;
     }
+
+//    for(Char c: arr){
+//        if(s.equals(targetValue))
+//            return true;
+//    }
+//	return false;
 
 
 }
